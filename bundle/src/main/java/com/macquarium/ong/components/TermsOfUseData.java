159793f807343816be603b63b07d08efdb2be520
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-08-08 10:31:55 -0400

    Terms Of Use Component

---
+++ bundle/src/main/java/com/macquarium/ong/components/TermsOfUseData.java
@@ -0,0 +1,24 @@
+package com.macquarium.ong.components;
+
+
+public class TermsOfUseData {
+
+    private String termsTitle;
+    private String termsDescription;
+
+    public String getTermsTitle() {
+        return termsTitle;
+    }
+
+    public void setTermsTitle(String termsTitle) {
+        this.termsTitle = termsTitle;
+    }
+
+    public String getTermsDescription() {
+        return termsDescription;
+    }
+
+    public void setTermsDescription(String termsDescription) {
+        this.termsDescription = termsDescription;
+    }
+}
