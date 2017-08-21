2e441c9fcd7df86009595561d05209a95e1b6afb
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-08-21 15:28:33 -0400

    Hero carousel: Updated for dynamic display of DSMAward.

--- bundle/src/main/java/com/macquarium/ong/components/CarouselSlide.java
+++ bundle/src/main/java/com/macquarium/ong/components/CarouselSlide.java
@@ -1,43 +1,44 @@
 package com.macquarium.ong.components;
 
 public class CarouselSlide {
-String title;
-String description;
-String imagePath;
-String buttonTitle;
-String buttonPath;
-String smallDescription;
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public String getDescription() {
-	return description;
-}
-public void setDescription(String description) {
-	this.description = description;
-}
+	String title;
+	String description;
+	String imagePath;
+	String buttonTitle;
+	String buttonPath;
+	String smallDescription;
+	String promoCode;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-public String getImagePath() {
-	return imagePath;
-}
-public void setImagePath(String imagePath) {
-	this.imagePath = imagePath;
-}
-public String getButtonTitle() {
-	return buttonTitle;
-}
-public void setButtonTitle(String buttonTitle) {
-	this.buttonTitle = buttonTitle;
-}
-public String getButtonPath() {
-	return buttonPath;
-}
-public void setButtonPath(String buttonPath) {
-	this.buttonPath = buttonPath;
-}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public String getButtonTitle() {
+		return buttonTitle;
+	}
+	public void setButtonTitle(String buttonTitle) {
+		this.buttonTitle = buttonTitle;
+	}
+	public String getButtonPath() {
+		return buttonPath;
+	}
+	public void setButtonPath(String buttonPath) {
+		this.buttonPath = buttonPath;
+	}
 
 	public String getSmallDescription() {
 		return smallDescription;
@@ -46,4 +47,12 @@ public void setButtonPath(String buttonPath) {
 	public void setSmallDescription(String smallDescription) {
 		this.smallDescription = smallDescription;
 	}
+	public String getPromoCode() {
+		return promoCode;
+	}
+	public void setPromoCode(String promoCode) {
+		this.promoCode = promoCode;
+	}
+
+
 }
--- bundle/src/main/java/com/macquarium/ong/components/HeroCarousel.java
+++ bundle/src/main/java/com/macquarium/ong/components/HeroCarousel.java
@@ -17,18 +17,19 @@ public class HeroCarousel extends WCMUsePojo {
 		String[] navItems = getProperties().get("iItems", String[].class);
 		slidesList=new ArrayList<CarouselSlide>();
 		if(null !=navItems)
-		for(String item:navItems){
-			CarouselSlide carouselSlide=new CarouselSlide();
-			JSONObject json=new JSONObject(item);
-			  	
+			for(String item:navItems){
+				CarouselSlide carouselSlide=new CarouselSlide();
+				JSONObject json=new JSONObject(item);
+
 				carouselSlide.setTitle((String)json.get("title"));
-			     carouselSlide.setDescription((String)json.get("description"));
-			     carouselSlide.setImagePath((String)json.get("imagepath"));
-			     carouselSlide.setButtonTitle((String)json.get("buttontitle"));
-			     carouselSlide.setButtonPath((String)json.get("buttonpath"));
-			     carouselSlide.setSmallDescription((String)json.get("smalldescription"));
-			     slidesList.add(carouselSlide);  
-		}		
+				carouselSlide.setDescription((String)json.get("description"));
+				carouselSlide.setImagePath((String)json.get("imagepath"));
+				carouselSlide.setButtonTitle((String)json.get("buttontitle"));
+				carouselSlide.setButtonPath((String)json.get("buttonpath"));
+				carouselSlide.setSmallDescription((String)json.get("smalldescription"));
+				carouselSlide.setPromoCode((String)json.get("promocode"));
+				slidesList.add(carouselSlide);
+			}
 	}
 	public List<CarouselSlide> getSlidesList() {
 		return slidesList;
@@ -36,6 +37,6 @@ public class HeroCarousel extends WCMUsePojo {
 	public void setSlidesList(List<CarouselSlide> slidesList) {
 		this.slidesList = slidesList;
 	}
-	
-	
+
+
 }
