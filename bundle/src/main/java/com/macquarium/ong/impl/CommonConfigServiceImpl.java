14f4bf8e8875437733d539174485031e04e1f2de
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-08-21 17:21:09 -0400

    Removed Production url from CommonConfigServiceImpl.java

--- bundle/src/main/java/com/macquarium/ong/impl/CommonConfigServiceImpl.java
+++ bundle/src/main/java/com/macquarium/ong/impl/CommonConfigServiceImpl.java
@@ -1,16 +1,11 @@
 package com.macquarium.ong.impl;
 
-import java.util.Dictionary;
-
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Modified;
-import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Service;
+import com.macquarium.ong.CommonConfigService;
+import org.apache.felix.scr.annotations.*;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.osgi.service.component.ComponentContext;
 
-import com.macquarium.ong.CommonConfigService;
+import java.util.Dictionary;
 
 @Component(immediate=true, metatype=true, name="OHGRE Common Configuration", label="OHGRE Common Configuration Service")
 @Service(value=com.macquarium.ong.CommonConfigService.class)
@@ -24,7 +19,7 @@ public class CommonConfigServiceImpl implements CommonConfigService {
 	private static final long serialVersionUID = 1L;
 	
 	
-	@Property(value="https://prime.southstarenergy.com/webservices/quoteservice.asmx?wsdl", label="Prime End Point Url", description="Prime End point url to communicate with prime", cardinality=0)
+	@Property(value="", label="Prime End Point Url", description="Prime End point url to communicate with prime", cardinality=0)
 	private static final String PRIME_ENDPOINT_URL = "prime.endpoint.url";	
 	
 		private String primeEndPoint=PRIME_ENDPOINT_URL;
