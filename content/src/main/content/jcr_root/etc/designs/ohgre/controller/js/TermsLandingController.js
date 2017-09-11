6a96a711a39a85b09a59b616e2869789c997e45d
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-09-11 00:36:18 -0400

    condition for back button on pages.

--- content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/TermsLandingController.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/TermsLandingController.js
@@ -1,13 +1,18 @@
 ohgrePortal.controller('TermsLandingController', ['$scope', '$rootScope', '$http', '$window',function ($scope, $rootScope,$http,$window) {
 
+     $scope.showfaq=true;
+
 
     $scope.goBack = function(){
-
         //$window.history.back();
 
         if(document.referrer) {window.open(document.referrer,'_self');} else {history.go(-1);} return false;
     }
-   
+    if(location.pathname.indexOf('faq')>0){
+
+        $scope.showfaq=false;
+
+    }
 
 }]);
 
