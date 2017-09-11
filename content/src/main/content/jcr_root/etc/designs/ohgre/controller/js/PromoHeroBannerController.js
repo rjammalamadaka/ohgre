29804530a7c89b845cb976ae8ce4c64469990523
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-09-11 00:40:59 -0400

    updates 'primeServices'. fixes for content on the promo hero banner.

--- content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/PromoHeroBannerController.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/PromoHeroBannerController.js
@@ -1,24 +1,59 @@
-ohgrePortal.controller('PromoHeroBannerController', ['$scope', '$rootScope', '$http',function ($scope, $rootScope,$http) {
+ohgrePortal.controller('PromoHeroBannerController', ['$scope', '$rootScope', '$http','PrimeService',function ($scope, $rootScope,$http,PrimeService) {
+
+
 
  var portalname=$rootScope.portalname;
 
      if($rootScope.hashParams.promocode){
-         var url="/bin/getPromoCodeInfo?portalName="+portalname+"&promotionCode="+$rootScope.hashParams.promocode;
-         $http.get(url).success(function(data, status, headers, config){
+         PrimeService.getPromoCodeInfo($rootScope.hashParams.promocode).success(function(data, status, headers, config){
              var promoInfo=ohgre.store("promoCodeInfo",data);
              $rootScope.promotionInfo=true;
+             if(data && data.responseStatus =="0"){
+
              if(promoInfo && promoInfo.LDCList.length>0){
                  var ldc=promoInfo.LDCList[0];
-                 $scope.promotion=ldc.promotion[0];
+                 $rootScope.promotion=ldc.promotion[0];
                  var date = new Date($scope.promotion.PromotionExpiratonDate),
                      locale = "en-us",
                      month = date.toLocaleString(locale, { month: "long" });
                  $scope.expdate=month+" "+date.getDate()+" ,"+date.getFullYear();
-                 
+
+                 if(promoInfo.PromotionExpired =="Y"){
+                                    location.href=$rootScope.homeUrl+"/backuppromo.html";
+                 }else{
+                     location.href=$rootScope.homeUrl+"/promo-general.html";
+                 }
+
              }
+             }else if(data && data.responseStatus =="1"){
+                 ohgre.store("promoCodeInfo",{});
+                            location.href=$rootScope.homeUrl+"/promotion-error.html";
+                     }
+
+  /*if(data.LDCList && data.LDCList.length >0){
+							//$window.sessionStorage.setItem('promoLDC',angular.toJson(data.LDCList));
+                            var ldclist= data.LDCList[0];
+                             var promotion=ldclist.promotion[0];
+                             ohgre.store("promoCodeInfo",data);
+                             if(promotion.PromotionExpired =="Y"){
+                                    location.href=$rootScope.homeUrl+"/backuppromo.html";
+                             }else if(data.LDCList && redirectUrl){
+                                    location.href=redirectUrl+".html";
+                            }else if(data.LDCList && data.LDCList.length!=0 && !redirectUrl){
+                                if(data.LDCList.length ==1){
+                                    location.href=$rootScope.homeUrl+"/promo-general.html";
+                                }else{
+                                    location.href=$rootScope.homeUrl+"/generic-promo.html";
+                                }
+                            }else{
+                                location.href=$rootScope.homeUrl+"/promo-general.html";
+                            }
+                         }*/
+
+
 
          }).error(function (data,status, headers, config){
-             
+
              console.log("error");
          });
 
@@ -26,16 +61,25 @@ ohgrePortal.controller('PromoHeroBannerController', ['$scope', '$rootScope', '$h
          var promoInfo=ohgre.store("promoCodeInfo");
             if(promoInfo && promoInfo.LDCList && promoInfo.LDCList.length>0){
                 var ldc=promoInfo.LDCList[0];
-                $scope.promotion=ldc.promotion[0];
+                $rootScope.promotion=ldc.promotion[0];
                 $rootScope.promotionInfo=true;        
                 var date = new Date($scope.promotion.PromotionExpiratonDate),
                 locale = "en-us",
                 month = date.toLocaleString(locale, { month: "long" });
                 $scope.expdate=month+" "+date.getDate()+" ,"+date.getFullYear();
-        
+                $scope.DELTAMILES=$scope.promotion.DSMAwardMiles;
+               $scope.GIFTCARDVALUE=$scope.promotion.GiftCardValue;
+
             }
 
      }
+    $scope.heightConstant = false;
+
+    if(location.pathname.indexOf("404")>0)
+    {
+		$scope.heightConstant = true;
+    }
+
 
     $scope.displayPromotionLogo =function(){
 
@@ -53,7 +97,28 @@ ohgrePortal.controller('PromoHeroBannerController', ['$scope', '$rootScope', '$h
             return "flush";
         }
     }
-    
+  var getPromoGroups= function(promotionCode){
+        OhGreService.getPromoGroups().success(function(data, status, headers, config){
+            for (var property in data) {
+    			if (data.hasOwnProperty(property) && property != "jcr:primaryType") {
+                    var childNode=data[property];
+                    for(var childproperty in childNode){
+                        if(childNode.hasOwnProperty(childproperty) && childproperty != "jcr:primaryType"){
+                            var url=childNode[childproperty];
+
+                            if(childproperty == promotionCode){
+							var promoinfo={};
+							promoinfo.code=childproperty;
+                            promoinfo.url=url;
+                            $scope.promoInfo=promoinfo;
+                                break;
+                            }
+                        }
+                    }
+    			}
+			}
+        }).error(function (data,status, headers, config){ });
+    }
 
 }]);
 
