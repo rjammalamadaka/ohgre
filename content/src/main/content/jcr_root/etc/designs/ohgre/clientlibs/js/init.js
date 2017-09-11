29dd334d8bb88bf947e25fa63b37265e60b08ba3
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-09-11 00:30:13 -0400

    1. issue 140 2. promolanding page URL issues. 3. LDC dropdown on promolanding pages issues

--- content/src/main/content/jcr_root/etc/designs/ohgre/clientlibs/js/init.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/clientlibs/js/init.js
@@ -1,4 +1,7 @@
-ohgrePortal.run(['$rootScope', '$compile', '$http', function ($rootScope, $compile, $http){
+ohgrePortal.run(['$rootScope', '$compile', '$http','PrimeService',"OhGreService", function ($rootScope, $compile, $http,PrimeService,OhGreService){ 
+    setTimeout(function(){ 
+        $('#portalbody').show();
+    }, 500);
 
      var portalname=$('#primary-header').data('portalname');
     $rootScope.portalname=portalname;
@@ -54,7 +57,7 @@ ohgrePortal.run(['$rootScope', '$compile', '$http', function ($rootScope, $compi
 
 		});
 
-       
+
 
     }
 
@@ -80,6 +83,7 @@ ohgrePortal.run(['$rootScope', '$compile', '$http', function ($rootScope, $compi
 
 		var ldc=null;
         var ldcbutton=$('#fixed-plans-button');
+        var promoInfo=ohgre.store("promoCodeInfo");
         if(ldcbutton && ldcbutton.length>0){
 			ldc=$(ldcbutton[0]).val();
         }
@@ -89,13 +93,38 @@ ohgrePortal.run(['$rootScope', '$compile', '$http', function ($rootScope, $compi
 					req.LdcDesc=$rootScope.ldcInfo.LDCDesc;
             }
         }else{
-           var promoInfo=ohgre.store("promoCodeInfo");
+
             if(promoInfo && promoInfo.LDCList && promoInfo.LDCList.length>0){
 				req.LDC=promoInfo.LDCList[0].LDCCode;
                 req.LdcDesc=promoInfo.LDCList[0].LDCDesc;
             }else{
 
             }
+
+        }
+
+        if(promoInfo && promoInfo.LDCList && promoInfo.LDCList.length>0 && promoInfo.LDCList[0].promotion && promoInfo.LDCList[0].promotion.length>0){
+			req.CustomerTypeCode=promoInfo.LDCList[0].promotion[0].CustomerTypeCode;
+			req.RateClassCode=promoInfo.LDCList[0].promotion[0].RateClassCode;	
+        }else{
+
+  			if($rootScope.hashParams && $rootScope.hashParams.lctype){
+                if($rootScope.hashParams.lctype =="residential"){
+					req.RateClassCode ="01";
+                }else if($rootScope.hashParams.lctype =="commercial"){
+					req.RateClassCode ="04";
+                }
+            }
+            if($rootScope.hashParams && $rootScope.hashParams.ldc){
+               var ldcType=$("input[name='location_type']:checked"). val(); 
+                if(ldcType=="residential"){
+					req.RateClassCode ="01";
+                }else if(ldcType =="commercial"){
+					req.RateClassCode ="04";
+                }
+
+            }
+            req.CustomerTypeCode="";
 
         }
 
@@ -134,3 +163,4 @@ $rootScope.currentYear=new Date().getFullYear();
     $rootScope.giftcardvalue="ddd";
 
 }]); 
+
