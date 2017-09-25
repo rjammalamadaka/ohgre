6100e8371f0f835706dd381cdc7abad946ace04d
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-09-25 09:36:16 -0400

    Updated with MySql configuration.

--- bundle/src/main/java/com/macquarium/ong/MovingServlet.java
+++ bundle/src/main/java/com/macquarium/ong/MovingServlet.java
@@ -1,12 +1,5 @@
 package com.macquarium.ong;
 
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.sling.SlingServlet;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.commons.json.JSONException;
-import org.apache.sling.commons.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.rmi.ServerException;
@@ -16,49 +9,57 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Calendar;
 
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
+
 @SlingServlet(paths="/bin/moving", methods = "get", metatype=true)
 public class MovingServlet extends org.apache.sling.api.servlets.SlingAllMethodsServlet{
 	private static final long serialVersionUID = 1L;
 	@Reference
-    private CommonConfigService commonConfigService;
+	private CommonConfigService commonConfigService;
 
-	 @Override
-     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
-		 process(request, response);
-	 }
-	 @Override
-     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
-		 process(request,response);
-	 }
-	 
-     private void process(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
-		 JSONObject obj=new JSONObject();
-		 Connection connection = null;
-		 String exceptionMessage=null;
-		 String resultCode=null;
-		 String resultMessage=null;
-		 StringBuilder sb = new StringBuilder();
-			BufferedReader br = request.getReader();
-			String str = null;
-			while ((str = br.readLine()) != null) {
-				sb.append(str);
-			}
-			JSONObject jObj = null;	
-			
-		 try {
-			 jObj = new JSONObject(sb.toString());
+	@Override
+	protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
+		process(request, response);
+	}
+	@Override
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
+		process(request,response);
+	}
+
+	private void process(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
+		JSONObject obj=new JSONObject();
+		Connection connection = null;
+		String exceptionMessage=null;
+		String resultCode=null;
+		String resultMessage=null;
+
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = request.getReader();
+		String str = null;
+		while ((str = br.readLine()) != null) {
+			sb.append(str);
+		}
+		JSONObject jObj = null;
+
+		try {
+			jObj = new JSONObject(sb.toString());
 			Class.forName("com.mysql.jdbc.Driver");
 			Calendar currentCalendar = Calendar.getInstance();
-			Calendar onemonthcalendar = Calendar.getInstance();			
+			Calendar onemonthcalendar = Calendar.getInstance();
 			onemonthcalendar.add(Calendar.MONTH, -1);
-			Calendar threemonthcalendar = Calendar.getInstance();			
+			Calendar threemonthcalendar = Calendar.getInstance();
 			threemonthcalendar.add(Calendar.MONTH, -3);
-			
-     	    java.sql.Date todayDate = new java.sql.Date(currentCalendar.getTime().getTime());
-     	   java.sql.Date movingOneMonthDate = new java.sql.Date(onemonthcalendar.getTime().getTime());
-     	  java.sql.Date movingThreeMonthDate = new java.sql.Date(threemonthcalendar.getTime().getTime());
-   	   
-     	    
+
+			java.sql.Date todayDate = new java.sql.Date(currentCalendar.getTime().getTime());
+			java.sql.Date movingOneMonthDate = new java.sql.Date(onemonthcalendar.getTime().getTime());
+			java.sql.Date movingThreeMonthDate = new java.sql.Date(threemonthcalendar.getTime().getTime());
+
+
 			connection = DriverManager.getConnection(commonConfigService.getMySqlConnectionUrl(),commonConfigService.getDataBaseUsername(), commonConfigService.getDataBasePassword());
 			if (connection != null) {
 				obj.put("got connection","You made it, take control your database now!");
@@ -70,23 +71,23 @@ public class MovingServlet extends org.apache.sling.api.servlets.SlingAllMethods
 				preparedStmt.setString(4, getParameterInfo(jObj,"lastname"));
 				preparedStmt.setString(5, getParameterInfo(jObj,"email"));
 				preparedStmt.setString(6, getParameterInfo(jObj,"phone"));
-				
+
 				String contactpermission=getParameterInfo(jObj,"contactpermission");
 				if(contactpermission.equalsIgnoreCase("yes")){
 					preparedStmt.setBoolean(7, true);
 				}else{
 					preparedStmt.setBoolean(7, false);
-				}		
-				
+				}
+
 				preparedStmt.setString(8, getParameterInfo(jObj,"hearaboutus"));
-				
+
 				String expectedmovedate=getParameterInfo(jObj,"expectedmovedate");
 				if(expectedmovedate.equals("Less than a month") || expectedmovedate.equals("1-3 months")){
 					preparedStmt.setDate(9, movingOneMonthDate);
 				}else{
 					preparedStmt.setDate(9, movingThreeMonthDate);
 				}
-				
+
 				preparedStmt.setString(10, getParameterInfo(jObj,"addressone"));
 				preparedStmt.setString(11, getParameterInfo(jObj,"addresstwo"));
 				preparedStmt.setString(12,  getParameterInfo(jObj,"addresscity"));
@@ -98,50 +99,50 @@ public class MovingServlet extends org.apache.sling.api.servlets.SlingAllMethods
 				}else{
 					preparedStmt.setBoolean(15,false );
 				}
-				
+
 				boolean b=preparedStmt.execute();
 				resultCode="0";
 				resultMessage="success";
-			} else {	
+			} else {
 				resultCode="1";
 				resultMessage="error";
 			}
 		}catch(ClassNotFoundException e){
 			resultCode="1";
 			resultMessage=e.getMessage();
-		}catch (JSONException e) {	
+		}catch (JSONException e) {
 			resultCode="1";
 			resultMessage=e.getMessage();
-		}catch (SQLException e) {		
+		}catch (SQLException e) {
 			resultCode="1";
 			resultMessage=e.getMessage();
 		}finally{
 			try {
 				connection.close();
-			} catch (SQLException e) {	
+			} catch (SQLException e) {
 				resultMessage=e.getMessage();
 			}
-		 }		
-		 
-		 try {
+		}
+
+		try {
 			obj.put("resultCode", resultCode);
-			obj.put("resultMessage", resultMessage);			
+			obj.put("resultMessage", resultMessage);
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			
-		 String jsonData = obj.toString();
-         response.getWriter().write(jsonData); 
-	 }
-     String getParameterInfo(JSONObject JObject, String parameter){
-     	String result="";
-     	try{
-     		result=JObject.getString(parameter);
-     	}catch(Exception e){
-     		
-     	}
-     	return result;
-     }
-     
+
+		String jsonData = obj.toString();
+		response.getWriter().write(jsonData);
+	}
+	String getParameterInfo(JSONObject JObject, String parameter){
+		String result="";
+		try{
+			result=JObject.getString(parameter);
+		}catch(Exception e){
+
+		}
+		return result;
+	}
+
 }
