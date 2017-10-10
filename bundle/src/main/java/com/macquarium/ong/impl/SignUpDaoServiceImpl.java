7586d8ff091772cce8872ac93f2724ef4bd204db
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-10-10 09:36:02 -0400

    Moving soon: updating existing customers details.

--- bundle/src/main/java/com/macquarium/ong/impl/SignUpDaoServiceImpl.java
+++ bundle/src/main/java/com/macquarium/ong/impl/SignUpDaoServiceImpl.java
@@ -3,6 +3,7 @@ package com.macquarium.ong.impl;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
 
@@ -68,7 +69,8 @@ public class SignUpDaoServiceImpl  implements SignUpDaoService{
                     preparedStmt.setBoolean(15,false );
                 }
 
-                result=preparedStmt.execute();
+                preparedStmt.execute();
+                result=true;
             }
         } catch(ClassNotFoundException e){
             e.printStackTrace();
@@ -76,8 +78,83 @@ public class SignUpDaoServiceImpl  implements SignUpDaoService{
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println(commonConfigService.getDataBaseUsername());
+        return result;
+    }
 
+
+    public boolean isExistingCustomer(Customer customer) {
+        boolean result=false;
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(commonConfigService.getMySqlConnectionUrl(),commonConfigService.getDataBaseUsername(), commonConfigService.getDataBasePassword());
+            if (connection != null) {
+                String email=customer.getEmail();
+                String query="SELECT * FROM signup where email=?";
+                PreparedStatement preparedStatement =connection.prepareStatement(query);
+                preparedStatement.setString(1, email);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    result=true;
+                }
+            }
+        }catch(Exception e){
+        }
+        return result;
+    }
+
+
+    public boolean updateCustomer(Customer customer) {
+        boolean result=false;
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(commonConfigService.getMySqlConnectionUrl(),commonConfigService.getDataBaseUsername(), commonConfigService.getDataBasePassword());
+            if (connection != null) {
+                Calendar currentCalendar = Calendar.getInstance();
+                Calendar onemonthcalendar = Calendar.getInstance();
+                onemonthcalendar.add(Calendar.MONTH, -1);
+                Calendar threemonthcalendar = Calendar.getInstance();
+                threemonthcalendar.add(Calendar.MONTH, -3);
+                java.sql.Date todayDate = new java.sql.Date(currentCalendar.getTime().getTime());
+                java.sql.Date movingOneMonthDate = new java.sql.Date(onemonthcalendar.getTime().getTime());
+                java.sql.Date movingThreeMonthDate = new java.sql.Date(threemonthcalendar.getTime().getTime());
+                String query="UPDATE signup SET type=?, created=?, first_name=?, last_name=?, phone_number=?, contact_via_phone=?, source=?,moving_date=?, address1=?, address2=?, city=?, state=?, zip=?, special_offers_optin=? where email=?";
+                PreparedStatement preparedStmt = connection.prepareStatement(query);
+                preparedStmt.setString(1, customer.getType());
+                preparedStmt.setDate(2, todayDate);
+                preparedStmt.setString(3, customer.getFirstName());
+                preparedStmt.setString(4, customer.getLastName());
+                preparedStmt.setString(5, customer.getPhoneNumber());
+                String contactpermission= customer.getContactViaPhone();
+                if(contactpermission.equalsIgnoreCase("yes")){
+                    preparedStmt.setBoolean(6, true);
+                }else{
+                    preparedStmt.setBoolean(6, false);
+                }
+                preparedStmt.setString(7, customer.getSource());
+                preparedStmt.setDate(8,customer.getMovingDate());
+                preparedStmt.setString(9,customer.getAddress1());
+                preparedStmt.setString(10,customer.getAddress2());
+                preparedStmt.setString(11, customer.getCity());
+                preparedStmt.setString(12, customer.getState());
+                preparedStmt.setString(13, customer.getZip());
+                String sendemails=customer.getSpecialOffersOption();
+                if(sendemails.equals("true")){
+                    preparedStmt.setBoolean(14,true) ;
+                }else{
+                    preparedStmt.setBoolean(14,false );
+                }
+                preparedStmt.setString(15, customer.getEmail());
+                int n= preparedStmt.executeUpdate();
+                System.out.println(n);
+                result=true;
+            }
+
+        }catch(Exception e){
+            System.out.println("exception while update the record");
+            e.printStackTrace();
+        }
         return result;
     }
 
