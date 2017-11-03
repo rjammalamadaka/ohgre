2392d16cdef972e0474ee46f4b7d767e169b655e
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-11-03 16:22:27 -0400

    renewal my plan and cancel my plan buttons functionality.

--- content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/DashBoardController.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/DashBoardController.js
@@ -21,7 +21,7 @@ ohgrePortal.controller('DashBoardController', ['$scope', '$rootScope', '$http' ,
 
             console.log($scope.customerInfo);
 
-            
+
         }).error(function(data, status, headers, config){
 
         });
@@ -33,11 +33,10 @@ ohgrePortal.controller('DashBoardController', ['$scope', '$rootScope', '$http' ,
          console.log();
 
          if(!isEmpty(data)){
-             
+             $scope.productData=data;
              var req={};
              req.AccountNumber=data.AccountNumber; 
              req.LDC=data.LDC;
-             
              getCustomerInfo(req);
          }else{
 			location.href=$rootScope.homeUrl+".html";
@@ -52,9 +51,54 @@ ohgrePortal.controller('DashBoardController', ['$scope', '$rootScope', '$http' ,
 
             location.href=$rootScope.homeUrl+".html";            
         }).error(function(data, status, headers, config){
-            
+
         });
-        
+
+    }
+
+    $scope.changeMyRenewal =function(){
+		location.href=$rootScope.homeUrl+'/select-plan.html';
+    }
+
+    $scope.cancelMyContract =function(){
+		location.href=$rootScope.homeUrl+'/cancel-contract.html';
+    }
+    $scope.getFormatedAccountnumber =function(accountNumber){
+
+        var formattedNumber="";
+        if(accountNumber){
+       console.log($scope.productData.LDC);
+		var ldc=$scope.productData.LDC;
+   if(ldc == "COH"){
+       //8331
+            formattedNumber=accountNumber.substring(0,8)+'-'+accountNumber.substring(8,11)+'-000-'+accountNumber.substring(14,15);
+
+        }else if(ldc == "DUK"){
+            //4421
+            formattedNumber=accountNumber.substring(0,4)+'-'+accountNumber.substring(4,8)+'-'+accountNumber.substring(8,10)+'-'+accountNumber.substring(10,11);
+
+        }else if(ldc == "DEO"){
+			//1444
+            formattedNumber=accountNumber.substring(0,1)+'-'+accountNumber.substring(1,5)+'-'+accountNumber.substring(5,9)+'-'+accountNumber.substring(9,13);
+
+        }else if(ldc == "VED"){
+            //2971
+			formattedNumber="03-"+accountNumber.substring(2,11)+"-"+accountNumber.substring(11,18)+"-0";
+
+        }else if(ldc == "MCG"){
+			//4341
+            formattedNumber=accountNumber.substring(0,4)+'-'+accountNumber.substring(4,7)+'-'+accountNumber.substring(7,11)+'-'+accountNumber.substring(11,12);
+
+        }else if(ldc == "MIC"){
+            formattedNumber=accountNumber;
+        }
+        return formattedNumber;
+
+        }else{
+			return "";
+        }
+
+
     }
 
 
