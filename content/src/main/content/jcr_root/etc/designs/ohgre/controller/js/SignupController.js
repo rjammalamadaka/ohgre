33d7963b97123d044df863afd16c6754155649f6
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-11-08 14:20:45 -0500

    SignUp controller for Moving soon program.

--- content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/SignupController.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/SignupController.js
@@ -1,83 +1,81 @@
-ohgrePortal.controller('SignupController', ['$scope', '$rootScope', '$http' ,'PrimeService','OhGreService',function ($scope, $rootScope,$http,PrimeService,OhGreService) {
+ohgrePortal.controller('SignupController', ['$scope', '$rootScope', '$http', 'PrimeService', 'OhGreService', function($scope, $rootScope, $http, PrimeService, OhGreService) {
 
 
-    $scope.signupsubmit= function(){
+  $scope.signupsubmit = function() {
 
+    $scope.signup.submited = true;
+    if ($scope.signup.$valid) {
+      document.getElementById("movingform").reset();
+      $rootScope.email = $scope.email;
 
-          $scope.signup.submited = true;
-        if($scope.signup.$valid){
-          document.getElementById("movingform").reset();
-          $rootScope.email  =$scope.email;
-
-				jQuery('#popup1').addClass("show-popup");
-        }
-
-
+      jQuery('#popup1').addClass("show-popup");
+      $('body').addClass('fixed-body');
     }
 
-    $('#close-window, .close-window-button').on('click',function(event){
-			event.preventDefault();
-        jQuery('#popup1').removeClass("show-popup");
+  }
 
-    });
+  $('#close-window, .close-window-button').on('click', function(event) {
+    event.preventDefault();
+    jQuery('#popup1').removeClass("show-popup");
+    $('body').removeClass('fixed-body');
+
+  });
 
 }]);
 
-ohgrePortal.controller('SignupPopupController', ['$scope', '$rootScope', '$http' ,'PrimeService','OhGreService',function ($scope, $rootScope,$http,PrimeService,OhGreService) {
+ohgrePortal.controller('SignupPopupController', ['$scope', '$rootScope', '$http', 'PrimeService', 'OhGreService', function($scope, $rootScope, $http, PrimeService, OhGreService) {
 
-    $scope.signupfornewsletter =function(){
-
-
-        $scope.movingform.submited = true;
-        if($scope.movingform.$valid){
+  $scope.signupfornewsletter = function() {
 
 
+    $scope.movingform.submited = true;
+    if ($scope.movingform.$valid) {
 
-            var req={};
 
-        req.firstname=$scope.firstname;
-		req.lastname=$scope.lastname;
-        req.email=$scope.email;
-       // req.phone="";
-	//	req.contactpermission="";
-        req.addressone=$scope.addressone;
-		req.addresstwo=$scope.addresstwo;
-        req.addresscity=$scope.addresscity;
-        req.addressstate=$scope.addressstate;
-        req.addresszip=$scope.addresszip;
-        req.sendemails=true;
 
-       // req.hearaboutus= "";
-        req.fromsignup=true;
-            if($rootScope.portalname =='oh'){
-				req.partner="ONG";
-            }else{
-				req.partner="GRE";
-            }   
+      var req = {};
 
-            PrimeService.moving(req).success(function(data, status, headers, config){
+      req.firstname = $scope.firstname;
+      req.lastname = $scope.lastname;
+      req.email = $scope.email;
+      // req.phone="";
+      //	req.contactpermission="";
+      req.addressone = $scope.addressone;
+      req.addresstwo = $scope.addresstwo;
+      req.addresscity = $scope.addresscity;
+      req.addressstate = $scope.addressstate;
+      req.addresszip = $scope.addresszip;
+      req.sendemails = true;
 
-                $scope.flag=false;
-                if(data.resultCode =="0"){
-                    jQuery('#popup1').removeClass("show-popup");
-					jQuery('#popup2').addClass("show-popup");
-                }
+      // req.hearaboutus= "";
+      req.fromsignup = true;
+      if ($rootScope.portalname == 'oh') {
+        req.partner = "ONG";
+      } else {
+        req.partner = "GRE";
+      }
 
-            }).error(function(data, status, headers, config){
+      PrimeService.moving(req).success(function(data, status, headers, config) {
 
-                $scope.flag=false;
-            });
-
+        $scope.flag = false;
+        if (data.resultCode == "0") {
+          jQuery('#popup1').removeClass("show-popup");
+          jQuery('#popup2').addClass("show-popup");
         }
+
+      }).error(function(data, status, headers, config) {
+
+        $scope.flag = false;
+      });
+
     }
+  }
 
 
-      $('#close-window, .close-window-button').on('click',function(event){
-			event.preventDefault();
-        jQuery('#popup2').removeClass("show-popup");
-          location.href=$rootScope.homeUrl+".html";
-    });
+  $('#close-window, .close-window-button').on('click', function(event) {
+    event.preventDefault();
+    jQuery('#popup2').removeClass("show-popup");
+    location.href = $rootScope.homeUrl + ".html";
+  });
 
 }]);
-
-
