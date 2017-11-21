60b14e5050db848abe48d0fef6a5d5ea49d959cc
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-11-21 16:13:49 -0500

    OH-477

--- content/src/main/content/jcr_root/etc/designs/ohgre/clientlibs/js/init.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/clientlibs/js/init.js
@@ -261,79 +261,13 @@ $rootScope.currentYear=new Date().getFullYear();
           });
       }
 
-   /* var getLdcInfo=function(){
-        PrimeService.getLdcInfo().success(function(data, status, headers, config){
-            if(data && data.responseStatus =="0"){
-                console.log(data.LDCList);
-                $rootScope.ldcinfo=data.LDCList;
-                setTimeout(function(){ $rootScope.loginPopupBindClickEvent(); }, 10);
-            }
 
-
-        }).error(function(data, status, headers, config){
-
-        });
-    }
-*/
-   /* $rootScope.myAccountCheck=function(url,name){
-
-		console.log(name);
-        if(name.indexOf('Account') != -1){
-            getLdcInfo();
-            jQuery("#login-popup-wrapper").addClass("show-popup");
-
-        }else{
-
-            location.href=url+".html";
+      $rootScope.isEmpty =function(obj) {
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key))
+                return false;
         }
+        return true;
     }
-*/
-
 }]); 
 
-/*
-
-(function (global) {
-
-    if(window.location.pathname.indexOf('customer_lookup.html') !=-1){
-
-    if(typeof (global) === "undefined") {
-        throw new Error("window is undefined");
-    }
-
-    var _hash = "!";
-    var noBackPlease = function () {
-        global.location.href += "#";
-
-        // making sure we have the fruit available for juice (^__^)
-        global.setTimeout(function () {
-            global.location.href += "!";
-        }, 50);
-    };
-
-    global.onhashchange = function () {
-        if (global.location.hash !== _hash) {
-            global.location.hash = _hash;
-        }
-    };
-
-    global.onload = function () {            
-        noBackPlease();
-
-        // disables backspace on page except on input fields and textarea..
-        document.body.onkeydown = function (e) {
-            var elm = e.target.nodeName.toLowerCase();
-            if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
-                e.preventDefault();
-            }
-            // stopping event bubbling up the DOM tree..
-            e.stopPropagation();
-        };          
-    }
-
-    }
-
-})(window);
-
-
-*/
