8f30338228372c70d3c71c8bc4dc82847d4b834f
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-11-21 15:55:00 -0500

    email notifications.

--- bundle/src/main/java/com/macquarium/ong/SendEmailService.java
+++ bundle/src/main/java/com/macquarium/ong/SendEmailService.java
@@ -1,5 +1,7 @@
 package com.macquarium.ong;
 
+import java.util.HashMap;
+
 public interface SendEmailService {
-	public boolean sendEmail(CommonConfigService commonConfigService, String soapRequest, String soapResponse);
+	public boolean sendEmail(HashMap<String,String> mailContent);
 }
