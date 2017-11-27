8c9f349779c765f5273c1c762f7b341076c501c4
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-11-27 13:35:48 -0500

    send exception email.

--- bundle/src/main/java/com/macquarium/ong/SendEmailService.java
+++ bundle/src/main/java/com/macquarium/ong/SendEmailService.java
@@ -4,4 +4,5 @@ import java.util.HashMap;
 
 public interface SendEmailService {
 	public boolean sendEmail(HashMap<String,String> mailContent);
+	public boolean sendExceptionEmail(HashMap<String,String> mailContent);
 }
