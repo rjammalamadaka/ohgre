fab6c54cd30d293a1f5678fbdb20ee6899df638c
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-11-27 09:53:24 -0500

    Storing request(xml) and response of the PRIME api calls. value object.

---
+++ bundle/src/main/java/com/macquarium/ong/vo/RequestResponseVo.java
@@ -0,0 +1,78 @@
+package com.macquarium.ong.vo;
+
+public class RequestResponseVo {
+	
+	String returnXML;
+	String respMessage;
+	String respNumb;
+	String accnt;
+	String postXML;
+	String apiCall;
+	String page;
+	String site;
+	String ldc;
+	String orderNumber;
+	
+	public String getReturnXML() {
+		return returnXML;
+	}
+	public void setReturnXML(String returnXML) {
+		this.returnXML = returnXML;
+	}
+	public String getRespMessage() {
+		return respMessage;
+	}
+	public void setRespMessage(String respMessage) {
+		this.respMessage = respMessage;
+	}
+	public String getRespNumb() {
+		return respNumb;
+	}
+	public void setRespNumb(String respNumb) {
+		this.respNumb = respNumb;
+	}
+	public String getAccnt() {
+		return accnt;
+	}
+	public void setAccnt(String accnt) {
+		this.accnt = accnt;
+	}
+	public String getPostXML() {
+		return postXML;
+	}
+	public void setPostXML(String postXML) {
+		this.postXML = postXML;
+	}
+	public String getApiCall() {
+		return apiCall;
+	}
+	public void setApiCall(String apiCall) {
+		this.apiCall = apiCall;
+	}
+	public String getPage() {
+		return page;
+	}
+	public void setPage(String page) {
+		this.page = page;
+	}
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+	public String getLdc() {
+		return ldc;
+	}
+	public void setLdc(String ldc) {
+		this.ldc = ldc;
+	}
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	
+
+}
