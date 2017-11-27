9f597be184a486eba596e5eb6fa385a4b1afe2a6
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-11-27 09:49:23 -0500

    Storing request(xml) and response of the PRIME api calls.

---
+++ bundle/src/main/java/com/macquarium/ong/RequestResponseDaoService.java
@@ -0,0 +1,8 @@
+package com.macquarium.ong;
+
+import com.macquarium.ong.vo.RequestResponseVo;
+
+public interface RequestResponseDaoService {
+
+	public boolean insertPrimeRequestResponse(RequestResponseVo requestResponseVo);
+}
