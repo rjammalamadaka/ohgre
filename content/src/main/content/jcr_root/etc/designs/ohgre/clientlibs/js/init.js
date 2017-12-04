6d7d480e1a6327b081eee0b0cdffd3f76502dd9d
avinash.dhannuri@macquarium.com <avinash.dhannuri@macquarium.com>
2017-12-04 00:53:52 -0500

    OH-470

--- content/src/main/content/jcr_root/etc/designs/ohgre/clientlibs/js/init.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/clientlibs/js/init.js
@@ -242,6 +242,24 @@ $rootScope.currentYear=new Date().getFullYear();
         }
     }
 
+    $rootScope.getNextDayDate =function(value){
+        if(value){
+        var date=new Date(value);
+        var dd = date.getDate()+1;
+        var mm = date.getMonth()+1; //January is 0!
+        var yyyy = date.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        } 
+        if(mm<10){
+            mm='0'+mm;
+        } 
+        return mm+'/'+dd+'/'+yyyy;
+        }else{
+		return "";
+        }
+    }
+
       $rootScope.getCustomerStatus=function(status){
 
         if(activeStates.indexOf(status) != -1){
