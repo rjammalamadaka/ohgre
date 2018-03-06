15c9593b938fcf25cb79fed79820250d07f6c40f
rjammalamadaka <jammrav@gmail.com>
2018-03-05 22:55:55 -0500

    fix for VISA amounts  OH-805

--- content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/ApplePromotionContentController.js
+++ content/src/main/content/jcr_root/etc/designs/ohgre/controller/js/ApplePromotionContentController.js
@@ -3,175 +3,176 @@ ohgrePortal.controller('ApplePromotionContentController', ['$scope', '$rootScope
 
     var portalname=$rootScope.portalname;
 
-  var processPromotionInfo=function(promoInfo,products){
+    var processPromotionInfo=function(promoInfo,products){
 
 
-              if(promoInfo && promoInfo.LDCList.length>0){
+        if(promoInfo && promoInfo.LDCList.length>0){
 
-                 /* var ldc=promoInfo.LDCList[0];
-                  var ldcCode=ldc.LDCCode;
-                  var promotion=ldc.promotion[0];
+            /* var ldc=promoInfo.LDCList[0];
+             var ldcCode=ldc.LDCCode;
+             var promotion=ldc.promotion[0];
 
-                  promotionCode=null;
-                  if(promotion && promotion.PromotionCode){
-                      if(promotion.PromotionExpired =="Y"){
-                          promotionCode= promotion.BackupPromotionCode; 
-                      }else{
-                          promotionCode=promotion.PromotionCode; 
+             promotionCode=null;
+             if(promotion && promotion.PromotionCode){
+             if(promotion.PromotionExpired =="Y"){
+             promotionCode= promotion.BackupPromotionCode;
+             }else{
+             promotionCode=promotion.PromotionCode;
 
-                      }
-                  }*/
-                  var ldc=null,promotionCode=null;
-                  for(var i=0;i<promoInfo.LDCList.length;i++){
-					 ldc=promoInfo.LDCList[i];
-                       var ldcCode=ldc.LDCCode;
-                      var promotion=ldc.promotion[0];
+             }
+             }*/
+            var ldc=null,promotionCode=null;
+            for(var i=0;i<promoInfo.LDCList.length;i++){
+                ldc=promoInfo.LDCList[i];
+                var ldcCode=ldc.LDCCode;
+                var promotion=ldc.promotion[0];
 
-                      if(promotion && promotion.PromotionCode){
-                           if( promotion &&  promotion.RateClassCode){
-                              $scope.rateClassCode=promotion.RateClassCode;
-                              $scope.giftCardValue=$scope.promotion.GiftCardValue;
-                          }
-                          if(promotion.PromotionExpired =="Y" && promotion.BackupPromotionCode.length>0){
-                              promotionCode= promotion.BackupPromotionCode; 
-                              break;
-                          }if(promotion.PromotionExpired =="N"){
-                              promotionCode=promotion.PromotionCode; 
-                              break;                              
-                          }
+                if(promotion && promotion.PromotionCode){
+                    $scope.giftCardValue=$scope.promotion.GiftCardValue;
+                    if( promotion &&  promotion.RateClassCode){
+                        $scope.rateClassCode=promotion.RateClassCode;
 
-
-                      }
-
-                  }
+                    }
+                    if(promotion.PromotionExpired =="Y" && promotion.BackupPromotionCode.length>0){
+                        promotionCode= promotion.BackupPromotionCode;
+                        break;
+                    }if(promotion.PromotionExpired =="N"){
+                        promotionCode=promotion.PromotionCode;
+                        break;
+                    }
 
 
+                }
 
-                  PrimeService.getQuotes(ldcCode,promotionCode,$scope.rateClassCode).success(function(data, status, headers, config){
-
-                      if(data && data.responseStatus =="0"){
-                          var customer=data.Customer[0];
-							var getQuotesProducts=customer.Product;
-
-							var getQuotesProductsList=[];
-
-                          for(var i=0;i<customer.Product.length;i++){
-                              if($rootScope.prmoProduct.indexOf(customer.Product[i].ProductCode) !=-1){
-								getQuotesProductsList.push(customer.Product[i]);
-
-                              }
-
-                          };
-
-                           $scope.productList=getQuotesProductsList;//customer.Product;
+            }
 
 
-                          updateProductFinePrint();
-                          setTimeout(function(){ $rootScope.bindAccordian(); }, 10);
-                      }
 
-                  }).error(function (data,status, headers, config){
+            PrimeService.getQuotes(ldcCode,promotionCode,$scope.rateClassCode).success(function(data, status, headers, config){
+
+                if(data && data.responseStatus =="0"){
+                    var customer=data.Customer[0];
+                    var getQuotesProducts=customer.Product;
+
+                    var getQuotesProductsList=[];
+
+                    for(var i=0;i<customer.Product.length;i++){
+                        if($rootScope.prmoProduct.indexOf(customer.Product[i].ProductCode) !=-1){
+                            getQuotesProductsList.push(customer.Product[i]);
+
+                        }
+
+                    };
+
+                    $scope.productList=getQuotesProductsList;//customer.Product;
 
 
-                  });
+                    updateProductFinePrint();
+                    setTimeout(function(){ $rootScope.bindAccordian(); }, 10);
+                }
 
-              }
+            }).error(function (data,status, headers, config){
+
+
+            });
+
+        }
 
 
 
     }
 
 
-  /*  $rootScope.$watch('promotionInfo', function (newValue, oldValue, scope) {
+    /*  $rootScope.$watch('promotionInfo', function (newValue, oldValue, scope) {
 
-        if(newValue){
+     if(newValue){
 
-			//var promoInfo=ohgre.store("promoCodeInfo");
-     		//processPromotionInfo(promoInfo);
-        }
-    });*/
+     //var promoInfo=ohgre.store("promoCodeInfo");
+     //processPromotionInfo(promoInfo);
+     }
+     });*/
 
-     $rootScope.$watch('prmoProduct', function (newValue, oldValue, scope) {
+    $rootScope.$watch('prmoProduct', function (newValue, oldValue, scope) {
 
         if(newValue && newValue.length>0){
-			var promoInfo=ohgre.store("promoCodeInfo");
+            var promoInfo=ohgre.store("promoCodeInfo");
 
-     		processPromotionInfo(promoInfo,newValue);
+            processPromotionInfo(promoInfo,newValue);
         }
     });
 
- if(!$rootScope.hashParams.promocode){
+    if(!$rootScope.hashParams.promocode){
 
-	//var promoInfo=ohgre.store("promoCodeInfo");
-   //  processPromotionInfo(promoInfo);
+        //var promoInfo=ohgre.store("promoCodeInfo");
+        //  processPromotionInfo(promoInfo);
 
 
- }
+    }
 
-var updateProductFinePrint = function() {
+    var updateProductFinePrint = function() {
 
-         angular.forEach($scope.productList, function(value, key) {
- 				var ProductFinePrintText = value.ProductFinePrintText.split(".");
-     			 var lastword = ProductFinePrintText[ProductFinePrintText.length - 1];
-      				if (!(lastword.length > 1))
-        		ProductFinePrintText.pop();
-      		$scope.productList[key].ProductFinePrintText = ProductFinePrintText;
-         });
+        angular.forEach($scope.productList, function(value, key) {
+            var ProductFinePrintText = value.ProductFinePrintText.split(".");
+            var lastword = ProductFinePrintText[ProductFinePrintText.length - 1];
+            if (!(lastword.length > 1))
+                ProductFinePrintText.pop();
+            $scope.productList[key].ProductFinePrintText = ProductFinePrintText;
+        });
 
-     }
+    }
 
 
     $scope.getNumber =function(QuoteDescription){
 
         if(QuoteDescription.indexOf("CCF")>0){
-			return("Ccf");
+            return("Ccf");
         }else if(QuoteDescription.indexOf("MCF")>0){
-			return("Mcf");
+            return("Mcf");
         }
 
-    } 
+    }
 
-  if($rootScope.hashParams.isExpired){
+    if($rootScope.hashParams.isExpired){
         $rootScope.expired=true;
     }
 
 
     $rootScope.$watch('ldcForEnrollPromo', function (newValue, oldValue, scope) {
-       if(newValue){
-		    //$scope.promotioncode = newValue.toUpperCase();
-           $rootScope.getPromoCodeInfoForEnroll($rootScope.ldcForEnrollPromo.LDCCode);
+        if(newValue){
+            //$scope.promotioncode = newValue.toUpperCase();
+            $rootScope.getPromoCodeInfoForEnroll($rootScope.ldcForEnrollPromo.LDCCode);
 
-       }
-	}, true);
+        }
+    }, true);
 
     $scope.displayAddlInfo = function(product) {
         if (product != undefined) {
 
-          if (product.displayAccordian == undefined) {
-            product.displayAccordian = true;
-          } else if (product.displayAccordian) {
-            product.displayAccordian = false;
-          } else if (!product.displayAccordian) {
-            product.displayAccordian = true;
-          }
+            if (product.displayAccordian == undefined) {
+                product.displayAccordian = true;
+            } else if (product.displayAccordian) {
+                product.displayAccordian = false;
+            } else if (!product.displayAccordian) {
+                product.displayAccordian = true;
+            }
         } else {
-          $scope.displayGuranteedAccord = $scope.displayGuranteedAccord ? false : true;
+            $scope.displayGuranteedAccord = $scope.displayGuranteedAccord ? false : true;
         }
 
         // alert(product.displayAccordian);
         //    $scope.displayAdditionalInfo = $scope.displayAdditionalInfo ? false : true;
 
-  	}
+    }
 
 
 
     $scope.getDisplayPromocode = function(promocode){
         if(promocode){
-			var index=promocode.indexOf("ONLINE");
+            var index=promocode.indexOf("ONLINE");
             if(index != -1){
-			return promocode.substr(0,index);
+                return promocode.substr(0,index);
             }else{
-				return promocode;
+                return promocode;
             }
         }else{ return "";
 
